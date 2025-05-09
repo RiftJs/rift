99333e06c3e9486a9200e673fd5bc6e0a6bb2672
Denes Feher <denesfhr@gmail.com>
2025-05-09 22:27:34 +0700

    no message

--- packages/rift/src/bootstrap.ts
+++ packages/rift/src/bootstrap.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 // Force TSLoader to use the correct tsconfig.json
-process.env.TS_NODE_PROJECT = require("path").resolve(__dirname, "../tsconfig.json");
+process.env.TS_NODE_PROJECT = require("path").resolve(__dirname, "../tsconfig.build.json");
 
 // bootstrap.ts (must be CommonJS, outside ESM)
 require("tsconfig-paths/register");
